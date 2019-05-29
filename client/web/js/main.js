bc1908f577c67edc99efeb5603de50f2e856b16e
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2019-05-29 17:06:49 +0200

    errors

--- client/web/js/main.js
+++ client/web/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  Dropzone.options.myAwesomeDropzone = { // The camelized version of the ID of the form element
+    /*
+  var dz = Dropzone.options.myAwesomeDropzone = { // The camelized version of the ID of the form element
     // The configuration we've talked about above
     url: '#',
     previewsContainer: ".dropzone-previews",
@@ -12,6 +13,29 @@ $(document).ready(function(){
 	    }
 	    else { done(); }
 	  }
-  }
+  };
+
+    dz.on("addedfile", function(file) {
+      // Hookup the start button
+      file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file); };
+    });
+*/
+
+
+    var myDropzone = new Dropzone(document.getElementById("my-awesome-dropzone"), { // Make the whole body a dropzone
+  url: "/target-url", // Set the url
+  thumbnailWidth: 80,
+  thumbnailHeight: 80,
+  parallelUploads: 20,
+  //previewTemplate: previewTemplate,
+  autoQueue: false, // Make sure the files aren't queued until manually added
+  previewsContainer: "#previews", // Define the container to display the previews
+  // clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+});
+
+myDropzone.on("addedfile", function(file) {
+  // Hookup the start button
+  file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file); };
+});
 
 });
