e09e7ee19d3938210cfb0b08117dbf371cbd5f5e
Serse <emanuele.raso@gmail.com>
2019-06-19 16:36:32 +0200

    Modifiche al front-end

--- client/static/js/main.js
+++ client/static/js/main.js
@@ -1,27 +1,44 @@
 //$(document).ready(function(){
+	
+    // Function to download data to a file
+    function download(data, filename, type) {
+    	var file = new Blob([data], {type: type});
+    	if (window.navigator.msSaveOrOpenBlob) // IE10+
+        	window.navigator.msSaveOrOpenBlob(file, filename);
+    	else { // Others
+        	var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        	a.href = url;
+        	a.download = filename;
+        	document.body.appendChild(a);
+        	a.click();
+        	setTimeout(function() {
+            		document.body.removeChild(a);
+            		window.URL.revokeObjectURL(url);  
+        	}, 0); 
+   	}
+    }
 
     Dropzone.options.encryptDropzone = {
             paramName: "file",
-            maxFilesize: 10,
+            maxFilesize: 1024,
             url: '/crypto/encrypt',
             previewsContainer: "#encrypt-dropzone-previews",
             uploadMultiple: true,
-            parallelUploads: 5,
-            maxFiles: 20,
+            parallelUploads: 1,
+            maxFiles: 1,
             init: function() {
-                var cd;
                 this.on("success", function(file, response) {
-                    $('.dz-progress').hide();
-                    $('.dz-size').hide();
-                    $('.dz-error-mark').hide();
+                    //$('.dz-progress').hide();
+                    //$('.dz-size').hide();
+                    //$('.dz-error-mark').hide();
                     console.log(response);
                     console.log(file);
-                    cd = response;
+                    download(atob(response), 'enc_file.txt', 'text/plain');
                 });
-                this.on("sending", function(file, xhr, o) {
+                this.on("sending", function(file) {
                   console.log(file);
                 });
-
                 this.on("drop", function (file) {
                   formdata = new FormData();
                   if($(this).prop('files').length > 0) {
@@ -33,34 +50,29 @@
                     url: '/crypto/encrypt',
                     data: formdata,
                     processData: false,
-                    contentType: false,
-                    success: function (result) {
-                         // if all is well
-                         // play the audio file
-                    }
+                    contentType: false
                   });
                 });
-
-                this.on("complete", function(file, resp) {
-                  console.log(resp);
-                });
-                this.on("addedfile", function(file) {
-                    var removeButton = Dropzone.createElement("<a href=\"#\">Remove file</a>");
-                    var _this = this;
-                    removeButton.addEventListener("click", function(e) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        _this.removeFile(file);
-                        var name = "largeFileName=" + cd.pi.largePicPath + "&smallFileName=" + cd.pi.smallPicPath;
-                        $.ajax({
-                            type: 'POST',
-                            url: 'DeleteImage',
-                            data: name,
-                            dataType: 'json'
-                        });
-                    });
-                    file.previewElement.appendChild(removeButton);
-                });
+                //this.on("complete", function(file, resp) {
+                //  console.log(resp);
+                //});
+                //this.on("addedfile", function(file) {
+                //    var removeButton = Dropzone.createElement("<a href=\"#\">Remove file</a>");
+                //    var _this = this;
+                //    removeButton.addEventListener("click", function(e) {
+                //        e.preventDefault();
+                //        e.stopPropagation();
+                //        _this.removeFile(file);
+                //        var name = "largeFileName=" + cd.pi.largePicPath + "&smallFileName=" + cd.pi.smallPicPath;
+                //        $.ajax({
+                //            type: 'POST',
+                //            url: 'DeleteImage',
+                //            data: name,
+                //            dataType: 'json'
+                //        });
+                //    });
+                //    file.previewElement.appendChild(removeButton);
+                //});
             }
     };
 
